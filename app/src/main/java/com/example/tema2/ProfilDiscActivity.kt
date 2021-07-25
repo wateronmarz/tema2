b43b52cdde1a49357311b42fff8dd5e9f73f7cf1
wateronmarz <radumarina.mr@gmail.com>
2021-07-25 19:04:06 +0300

    profil disciplina

---
+++ app/src/main/java/com/example/tema2/ProfilDiscActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tema2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfilDiscActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activityprofildisc_main)
+    }
+}
